b9f4df135649dd3b17180bb1df7b6ffe349f87f3
Carolina <166563593+Carolinac07@users.noreply.github.com>
2025-11-09 19:03:15 -0300

    Add files via upload

--- db/db.py
+++ db/db.py
@@ -7,10 +7,19 @@ class DB:
     def AgregarAnimal(self, animal: Animal, cursor:psycopg.Cursor):
        cursor.execute("INSERT INTO animales (nombre, especie, edad) VALUES (%s,%s,%s)", (animal.nombre, animal.especie, animal.edad))
        return {"msg" : "Animal agregado."}
-
-    def ObtenerAnimal(self, cursor:psycopg.Cursor):
-        cursor.execute("SELECT * FROM animales")
-        return {"msg" : "Animales obtenidos."}
+    
+    def ObtenerAnimal(self, cursor: psycopg.Cursor):
+        cursor.execute("SELECT id_animal, nombre, especie, edad FROM animales")
+        rows = cursor.fetchall()
+        return [
+        {
+            "id_animal": row[0],
+            "nombre": row[1],
+            "especie": row[2],
+            "edad": row[3]
+        }
+        for row in rows
+    ]
     
     def EliminarAnimal(self, animal: Animal, cursor:psycopg.Cursor):
         cursor.execute("DELETE FROM animales WHERE nombre = %s AND especie = %s AND edad = %s", (animal.nombre, animal.especie, animal.edad))
@@ -21,10 +30,18 @@ class DB:
     def AgregarCuidador(self, cuidador: Cuidador, cursor:psycopg.Cursor):
         cursor.execute("INSERT INTO cuidadores (nombre, edad) VALUES (%s,%s)", (cuidador.nombre, cuidador.edad))
         return {"msg" : "Cuidador agregado."}
-
-    def ObtenerCuidador(self, cursor:psycopg.Cursor):
-        cursor.execute("SELECT * FROM cuidadores")
-        return {"msg" : "Cuidadores obtenidos."}
+    
+    def ObtenerCuidador(self, cursor: psycopg.Cursor):
+        cursor.execute("SELECT id_cuidador, nombre, edad FROM cuidadores")
+        rows = cursor.fetchall()
+        return [
+        {
+            "id_cuidador": row[0],
+            "nombre": row[1],
+            "edad": row[2]
+        }
+        for row in rows
+    ]
     
     def EliminarCuidador(self, id_cuidador: int, cursor: psycopg.Cursor):
         cursor.execute("DELETE FROM cuidadores WHERE id_cuidador = %s", (id_cuidador,))
@@ -36,9 +53,18 @@ class DB:
         cursor.execute("INSERT INTO habitats (nombre, id_animal, id_cuidador) VALUES (%s,%s,%s)", (habitat.nombre, habitat.id_animal, habitat.id_cuidador))
         return {"msg" : "Habitat agregado."}
     
-    def ObtenerHabitat(self, cursor:psycopg.Cursor):
-        cursor.execute("SELECT * FROM habitats")
-        return {"msg" : "Habitats obtenidos."}
+    def ObtenerHabitat(self, cursor: psycopg.Cursor):
+        cursor.execute("SELECT id_habitat, nombre, id_animal, id_cuidador FROM habitats")
+        rows = cursor.fetchall()
+        return [
+        {
+            "id_habitat": row[0],
+            "nombre": row[1],
+            "id_animal": row[2],
+            "id_cuidador": row[3]
+        }
+        for row in rows
+    ]
     
     def ActualizarHabitat(self, habitat_id: int, habitat: Habitat, cursor: psycopg.Cursor):
         cursor.execute(
